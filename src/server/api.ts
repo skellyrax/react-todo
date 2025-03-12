071324a05e7990c83c45ea3e7bd8175a74255811
Ryan J Helm <ryjhelm@gmail.com>
2025-03-12 10:44:32 -0600

    added db capability for deployed application to use and finished prep for deploying with railway

--- src/server/api.ts
+++ src/server/api.ts
@@ -2,10 +2,16 @@ import { remultExpress } from 'remult/remult-express'
 import { Task } from '../shared/Task.js'
 import { TasksController } from '../shared/TasksController.js'
 import { AuthController } from '../shared/AuthController.js'
+import { createPostgresDataProvider } from 'remult/postgres'
+
+const DATABASE_URL = process.env["DATABASE_URL"];
 
 export const api = remultExpress({
     entities: [Task],
     admin: true, // enable admin ui
     controllers: [TasksController, AuthController], // add custom controller
     getUser: (req) => req.session!['user'], // use session to get user
+    dataProvider: DATABASE_URL
+        ? createPostgresDataProvider({ connectionString: DATABASE_URL })
+        : undefined, // use postgres data provider with your connection string  
 })
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import { api } from './api.js';
 import session from "cookie-session"
+// import { auth } from './auth.js'
 
 const app = express()
 app.use(
     session({ secret: process.env["SESSION_SECRET"] || "my secret" })
 )
 app.use(api);
-app.listen(3002, () => console.log('server running'));
+// app.use(auth)
+
+const frontendFiles = process.cwd() + "/dist";
+app.use(express.static(frontendFiles));
+app.get("/*", (_, res) => {
+    res.sendFile(frontendFiles + "/index.html");
+});
+app.listen(process.env["PORT"] || 3002, () => console.log("Server started"));
